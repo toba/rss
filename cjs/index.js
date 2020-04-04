af094704067d2f8505e5199accddcd3ae65af878
Jason Abbott <dev@trailimage.com>
2020-04-04 11:39:14 -0600

    remove build artifacts

--- cjs/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var atom_1 = require("./atom");
-exports.render = atom_1.render;
--- esm/index.d.ts
+++
@@ -1,2 +0,0 @@
-export { render } from './atom';
-export { Entry as AtomEntry, Feed as AtomFeed, Person as AtomPerson, Generator as AtomGenerator, Link as AtomLink, Text as AtomText, TextType as AtomTextType, Category as AtomCategory, ISyndicate } from './types';
